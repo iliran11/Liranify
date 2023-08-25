61e762dcf64de24f4d597a04d62d259694c9581f
Liran Cohen <iliran11@gmail.co,m>
2023-08-25 08:27:38 +0300

    Add id to each heading

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,12 +4,14 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
+    .use(rehypeSlug)
     .use(rehypeHighlight)
     .use(rehypeStringify)
     .process(markdown);
